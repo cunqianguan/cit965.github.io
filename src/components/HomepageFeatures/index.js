9541ca32ebd1959de132c931f5fa21a3c861c6c5
mouuii <49775493+mouuii@users.noreply.github.com>
2023-04-24 12:03:35 +0800

    second commit

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: '最好的教程',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -14,7 +14,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: '最温暖的社区',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
@@ -24,7 +24,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
+    title: '拒绝996',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
